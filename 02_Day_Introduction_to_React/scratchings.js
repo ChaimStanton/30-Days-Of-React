dbf68dbbea1b2e63a6612d53da457794160c9cb1
Chaim Stanton <38401049+ChaimStanton@users.noreply.github.com>
2022-05-31 01:25:03 +0100

    day 2 exes

---
+++ 02_Day_Introduction_to_React/scratchings.js
@@ -0,0 +1,9 @@
+const header = (
+    <header>
+        <h1>Welcome to 30 Days Of React</h1>
+        <h2>Getting Started React</h2>
+        <h3>JavaScript Library</h3>
+    </header>
+)
+
+const jsxElement = <h1>I am a JSX element</h1>
