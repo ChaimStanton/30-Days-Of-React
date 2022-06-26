20cbf834db3afea15a738875bc025fd5d246ceef
Chaim Stanton <38401049+ChaimStanton@users.noreply.github.com>
2022-06-26 15:17:00 +0100

    did a bit of messing day 7

--- 07_Day_Class_Components/day7messing/src/index.js
+++ 07_Day_Class_Components/day7messing/src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//index.js
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import React from "react";
+import ReactDOM from "react-dom";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// class based component
+class Header extends React.Component {
+  greetPeople = () => {
+    alert("Welcome to 30 Days Of React Challenge, 2020");
+  };
+  render() {
+    return (
+      <header>
+        <div className="header-wrapper">
+          <h1>Welcome to 30 Days Of React</h1>
+          <h2>Getting Started React</h2>
+          <h3>JavaScript Library</h3>
+          <p>Asabeneh Yetayeh</p>
+          <small>Oct 7, 2020</small>
+          <button onClick={this.greetPeople}> Greet </button>
+        </div>
+      </header>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Header />, rootElement);
